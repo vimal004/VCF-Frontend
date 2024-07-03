93709155267d23f7afa00d4007a0fb98ae3e10d6
vimal004 <2004.vimal@gmail.com>
2024-07-03 15:05:49 +0530

    14

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,71 +4,123 @@ import { AppBar, Toolbar, Typography, Box, Button } from "@mui/material";
 import {
   People as PeopleIcon,
   Group as GroupIcon,
-  Warning as WarningIcon,
+  Visibility as VisibilityIcon,
+  ReportProblem as ReportProblemIcon,
 } from "@mui/icons-material";
 import logo from "./img/logo.png";
+import { styled } from "@mui/system";
 
 const Header = () => {
   return (
-    <AppBar position="static" color="default" elevation={2}>
+    <StyledAppBar position="static" color="default" elevation={2}>
       <Toolbar>
-        <Box display="flex" alignItems="center" flexGrow={1}>
-          <Link to="/">
-            <img src={logo} alt="Logo" style={{ height: "48px" }} />
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Logo src={logo} alt="Logo" />
+            <Typography
+              variant="h6"
+              color="inherit"
+              sx={{ ml: 2, fontWeight: 600 }}
+            >
+              Customer Database Management
+            </Typography>
           </Link>
-          <Typography
-            variant="h6"
-            color="inherit"
-            style={{ marginLeft: "16px" }}
-          >
-            Customer Database Management
-          </Typography>
+          <Navigation>
+            <AnimatedButton
+              component={Link}
+              to="/allcustomers"
+              color="inherit"
+              variant="text"
+              sx={{ fontWeight: 500 }}
+              startIcon={<PeopleIcon />}
+            >
+              View Customers
+            </AnimatedButton>
+            <AnimatedButton
+              component={Link}
+              to="/managecustomers"
+              color="inherit"
+              variant="text"
+              sx={{ fontWeight: 500 }}
+              startIcon={<PeopleIcon />}
+            >
+              Manage Customers
+            </AnimatedButton>
+            <AnimatedButton
+              component={Link}
+              to="/managegroups"
+              color="inherit"
+              variant="text"
+              sx={{ fontWeight: 500 }}
+              startIcon={<GroupIcon />}
+            >
+              Manage Groups
+            </AnimatedButton>
+            <AnimatedButton
+              component={Link}
+              to="/viewgroups"
+              color="inherit"
+              variant="text"
+              sx={{ fontWeight: 500 }}
+              startIcon={<GroupIcon />}
+            >
+              View Groups
+            </AnimatedButton>
+            <AnimatedButton
+              component={Link}
+              to="/defaulters"
+              color="inherit"
+              variant="text"
+              sx={{ fontWeight: 500 }}
+              startIcon={<ReportProblemIcon />}
+            >
+              View Defaulters
+            </AnimatedButton>
+          </Navigation>
         </Box>
-        <nav>
-          <Button
-            component={Link}
-            to="/allcustomers"
-            color="inherit"
-            startIcon={<PeopleIcon />}
-          >
-            View Customers
-          </Button>
-          <Button
-            component={Link}
-            to="/managecustomers"
-            color="inherit"
-            startIcon={<PeopleIcon />}
-          >
-            Manage Customers
-          </Button>
-          <Button
-            component={Link}
-            to="/managegroups"
-            color="inherit"
-            startIcon={<GroupIcon />}
-          >
-            Manage Groups
-          </Button>
-          <Button
-            component={Link}
-            to="/viewgroups"
-            color="inherit"
-            startIcon={<GroupIcon />}
-          >
-            View Groups
-          </Button>
-          <Button
-            component={Link}
-            to="/defaulters"
-            color="inherit"
-            startIcon={<WarningIcon />}
-          >
-            View Defaulters
-          </Button>
-        </nav>
       </Toolbar>
-    </AppBar>
+    </StyledAppBar>
   );
 };
+
+const StyledAppBar = styled(AppBar)({
+  backgroundColor: "#ffffff",
+  boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.1)",
+  borderRadius: "8px",
+});
+
+const Logo = styled("img")({
+  height: "40px",
+  borderRadius: "50%",
+  marginRight: "12px",
+});
+
+const Navigation = styled("nav")({
+  display: "flex",
+  alignItems: "center",
+});
+
+const AnimatedButton = styled(Button)({
+  transition: "background-color 0.3s, color 0.3s",
+  borderRadius: "8px",
+  margin: "0 8px",
+  padding: "8px 16px",
+  fontSize: "0.9rem",
+  "&:hover": {
+    backgroundColor: "#f0f0f0",
+  },
+});
 
 export default Header;
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import {
-  AccountCircle,
-  People,
-  Group,
-  Visibility,
-  ReportProblem,
+  AccountCircle as AccountCircleIcon,
+  People as PeopleIcon,
+  Group as GroupIcon,
+  Visibility as VisibilityIcon,
+  ReportProblem as ReportProblemIcon,
 } from "@mui/icons-material";
 
 const Home = () => {
@@ -40,102 +40,97 @@ const Home = () => {
         padding: "24px",
       }}
     >
-      <Grid container spacing={3} direction="column" alignItems="center">
-        <Grid item xs={12}>
+      <Grid container spacing={3} justifyContent="center">
+        <Grid item xs={12} sm={6} md={4}>
           <Link
             to="/allcustomers"
             style={{
               textDecoration: "none",
-              display: "block",
-              width: "100%",
-              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <Button
               variant="contained"
               color="primary"
               sx={buttonStyles}
-              startIcon={<AccountCircle sx={iconStyles} />}
+              startIcon={<AccountCircleIcon sx={iconStyles} />}
             >
               View Customers Details
             </Button>
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6} md={4}>
           <Link
             to="/managecustomers"
             style={{
               textDecoration: "none",
-              display: "block",
-              width: "100%",
-              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <Button
               variant="contained"
               color="success"
               sx={buttonStyles}
-              startIcon={<People sx={iconStyles} />}
+              startIcon={<PeopleIcon sx={iconStyles} />}
             >
               Manage Customer Records
             </Button>
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6} md={4}>
           <Link
             to="/managegroups"
             style={{
               textDecoration: "none",
-              display: "block",
-              width: "100%",
-              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <Button
               variant="contained"
               color="error"
               sx={buttonStyles}
-              startIcon={<Group sx={iconStyles} />}
+              startIcon={<GroupIcon sx={iconStyles} />}
             >
               Manage Groups
             </Button>
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6} md={4}>
           <Link
             to="/viewgroups"
             style={{
               textDecoration: "none",
-              display: "block",
-              width: "100%",
-              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <Button
               variant="contained"
               color="warning"
               sx={buttonStyles}
-              startIcon={<Visibility sx={iconStyles} />}
+              startIcon={<VisibilityIcon sx={iconStyles} />}
             >
               View Groups
             </Button>
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6} md={4}>
           <Link
             to="/defaulters"
             style={{
               textDecoration: "none",
-              display: "block",
-              width: "100%",
-              marginBottom: "16px",
+              display: "flex",
+              justifyContent: "center",
             }}
           >
             <Button
               variant="contained"
               color="secondary"
               sx={buttonStyles}
-              startIcon={<ReportProblem sx={iconStyles} />}
+              startIcon={<ReportProblemIcon sx={iconStyles} />}
             >
               View Defaulters By Group
             </Button>
